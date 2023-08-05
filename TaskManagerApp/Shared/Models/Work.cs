160a1c7a91a5e9dc7eb3ee192d37c574d9a14a87
atsi26 <emmanuelatikese14@gmail.com>
2023-08-05 23:53:50 +0000

    Adding updates

---
+++ TaskManagerApp/Shared/Models/Work.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerApp.Shared.Models
+{
+    public class Work
+    {
+        public List<TaskItem> WorkItem { get; set; } = new List<TaskItem>();    
+    }
+}
